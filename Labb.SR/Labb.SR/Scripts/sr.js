089d0c12c46287ba8af734044fea734caf4271f0
David Blomberg <david.blomberg@gmail.com>
2012-01-23 21:49:10 +0100

    read xml with jquery

--- Labb.SR/Labb.SR/Scripts/sr.js
+++ Labb.SR/Labb.SR/Scripts/sr.js
@@ -1,11 +1,8 @@
 ﻿
 /// <reference path="2011.3.1129/kendo.all.min.js" />
 
-function divide(a, b) {
-    return a / b;
-}
 
 function getTablå() {
-            //var datasource=kendo
+            
 
 }
